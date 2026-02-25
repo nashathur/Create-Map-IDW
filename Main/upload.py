be7d510ee72fd28d4153acf19fde3f76973dd2e0
Thursina Ahsan <126236905+nashathur@users.noreply.github.com>
2026-02-25 13:03:31 +0100

    Update upload.py

--- Main/upload.py
+++ Main/upload.py
@@ -57,7 +57,8 @@ def _upload_local(cfg, status_update):
 
     root = tk.Tk()
     root.withdraw()
-
+    root.attributes('-topmost', True)
+    
     jenis = cfg.jenis_peta
     filetypes = [("Excel/CSV files", "*.xlsx *.xls *.csv"), ("All files", "*.*")]
 
